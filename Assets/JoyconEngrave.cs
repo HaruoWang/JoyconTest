ab55c1fa6047f38f44520a8972806f6616e2c14e
HW <wang355152@gmail.com>
2025-08-28 15:11:13 +0800

    can del wall with trigger

--- Assets/JoyconEngrave.cs
+++ Assets/JoyconEngrave.cs
@@ -1,6 +1,7 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class JoyconEngrave : MonoBehaviour
 {
     private List<Joycon> joycons;
@@ -27,6 +28,10 @@ public class JoyconEngrave : MonoBehaviour
     public Rigidbody p;
     public float _speed;
 
+    // public RectTransform cursorUI;
+
+    public GameObject cursor;
+
 
     void Start()
     {
@@ -46,28 +51,31 @@ public class JoyconEngrave : MonoBehaviour
 
     void Update()
     {
-        if (ggyro >= 20 && go == false)
-        {
-            stepCount += 1;
-            if (p)
-            {
-                p.AddRelativeForce(Vector3.forward * _speed);
-            }
+        // if (ggyro >= 20 && go == false)
+        // {
+        //     stepCount += 1;
+        //     if (p)
+        //     {
+        //         p.AddRelativeForce(Vector3.forward * _speed);
+        //     }
 
-            go = true;
-        }
+        //     go = true;
+        // }
 
-        if (ggyro < 20 && go == true)
-        {
-            //stepCount += 1;
-            go = false;
-        }
-
-        // Debug.Log(new Vector2(joycons[jc_ind].GetStick()[0], joycons[jc_ind].GetStick()[1]));
+        // if (ggyro < 20 && go == true)
+        // {
+        //     go = false;
+        // }
 
         if (joycons.Count == 0) return;
 
         Joycon j = joycons[jcIndex];
+
+        gyro = j.GetGyro();
+
+        float GetGyroX = gyro[0];
+        float GetGyroY = gyro[1] * -1;
+        float GetGyroZ = gyro[2];
 
         if (j.GetButtonDown(Joycon.Button.SHOULDER_2))
         {
@@ -77,17 +85,59 @@ public class JoyconEngrave : MonoBehaviour
         }
 
         float[] stickArr = j.GetStick();
-        Vector2 stick = new Vector2(stickArr[0], stickArr[1]);
-        int x = Mathf.Clamp(Mathf.RoundToInt((stick.x + 1f) / 2f * (gridSize - 1)), 0, gridSize - 1);
-        int y = Mathf.Clamp(Mathf.RoundToInt((stick.y + 1f) / 2f * (gridSize - 1)), 0, gridSize - 1);
-        int z = gridSize / 2;
+        float stickX = stickArr[0];
+        float stickY = stickArr[1];
+        float cursorX = (stickX + 1f) / 2f * Screen.width;
+        float cursorY = (stickY + 1f) / 2f * Screen.height;
+        cursorY = Screen.height - cursorY;
+
+        //orientation = j.GetVector();
+        //rot = orientation.eulerAngles;
+        //float X = (float)Math.Round(orientation[0], 1);
+        //float Y = (float)Math.Round(orientation[1], 1);
+        //float Z = (float)Math.Round(orientation[2], 1);
+        //float X = rot[0] / 360;
+        //float Y = rot[1] / 360;
+        //float Z = rot[2] / 360;
+
+        //Debug.Log($"{rot}");
+
+        //Debug.Log($"陀螺儀： 0, 0, 0");
+        //Debug.Log($"{orientation}");
+        // Debug.Log($"Cursor Position: {cursorX}, {cursorY}");
+        // 目前按著b2 cursor，座標有反應，等等改
+        // Vector2 stick = new Vector2(stickArr[0], stickArr[1]);
+        // int x = Mathf.Clamp(Mathf.RoundToInt((stick.x + 1f) / 2f * (gridSize - 1)), 0, gridSize - 1);
+        // int y = Mathf.Clamp(Mathf.RoundToInt((stick.y + 1f) / 2f * (gridSize - 1)), 0, gridSize - 1);
+        // int z = gridSize / 2;
+
+        // cursorUI.position = new Vector2(cursorX, cursorY);
+        //cursor.transform.position = new Vector3(X, Y, Z);
+
+        Quaternion q = j.GetVector();
+
+        // 取得三軸方向
+        Vector3 forward = q * Vector3.forward;
+        Vector3 up = q * Vector3.up;
+        Vector3 right = q * Vector3.right * -1;
+
+        float x = stickX * 3.5f - 0.3f;
+        float y = stickY;
+        float z = forward.z * 5f + 2f;
+
+        //Vector3 p = (forward + up + right) / 3;
+
+        // 假設游標基準點在 (0,0,0)，讓它隨著 forward 移動
+        cursor.transform.position = new Vector3(x, y, z);
+
+        // Mouse.current.WarpCursorPosition(new Vector2(cursorX, cursorY));
+
+        // Debug.Log(cursor.transform.position);
 
         if (j.GetButtonDown(Joycon.Button.SHOULDER_2))
         {
-            CarveVoxel(x, y, z);
+            // CarveVoxel(x, y, z);
         }
-        
-        // stick = j.GetStick();
 
             gyro = j.GetGyro();
             accel = j.GetAccel();
@@ -106,6 +156,12 @@ public class JoyconEngrave : MonoBehaviour
                 gameObject.GetComponent<Renderer>().material.color = Color.blue;
             }
             gameObject.transform.rotation = orientation;
+    }
+
+    void OnTriggerEnter(Collider wall)
+    {
+        Debug.Log("Hit the wall");
+        GameObject.Destroy(wall.gameObject);
     }
 
     void CarveVoxel(int x, int y, int z)
